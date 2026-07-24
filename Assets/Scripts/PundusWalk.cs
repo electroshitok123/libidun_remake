cccc613bdcafc57774d9d02d60b2ed72ef09a74e
electroshitok123 <pngkovrik@gmail.com>
2026-07-24 13:08:50 +0300

    clearProject
    пундус удален, осталась только земля

--- Assets/Scripts/PundusWalk.cs
+++ Assets/Scripts/PundusWalk.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class PundusWalk : MonoBehaviour
 {
-    //[SerializeField] private float speed = 3f;
-    //[SerializeField] private float jumpForce = 15f;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float jumpForce = 15f;
+    private float movX;
+    private float movY;
 
-    //private Rigidbody2D rb;
-    //private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
-    //private void Awake()
-    //{
-    //    rb = GetComponent<Rigidbody2D>();
-    //    sprite = GetComponentInChildren<SpriteRenderer>();
-    //}
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        movX = 0;
+        movY = 0;
+    }
 
+    private void Update()
+    {
 
+    }
 }
