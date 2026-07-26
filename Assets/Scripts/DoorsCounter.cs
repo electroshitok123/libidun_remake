e6170fe9a81dcc359f3c3e0bea2615c646a3ee0b
Lazy-Crab <sashapashnina2007@gmail.com>
2026-07-26 17:32:46 +0300

    Переход
    Сделан переход на следующий уровень

--- Assets/Scripts/DoorsCounter.cs
+++ Assets/Scripts/DoorsCounter.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorsCounter : MonoBehaviour
 {
     public int doors;
+
+    private void Update()
+    {
+        LoadNextScene();
+    }
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+        if (doors == 3)
+        {
+            SceneManager.LoadScene(nextIndex);
+        }
+    }
 
     public void AddScore(int amount)
     {
